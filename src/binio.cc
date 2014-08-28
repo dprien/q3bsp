376c51ced1bda3cc1a13236eeeb4c17b7e09d730
Dominik Prien <dprien@users.noreply.github.com>
2014-08-28 17:20:59 +0200

    Add binio.{cc,h}, binary IO helper/wrapper class

---
+++ src/binio.cc
@@ -0,0 +1,170 @@
+#include <memory>
+#include <cstring>
+
+#include <SDL2/SDL.h>
+
+#include "binio.h"
+#include "exception.h"
+
+BinaryIO::BinaryIO(BinaryIO::octet_vec_t octets)
+    : m_octets(std::move(octets))
+{
+    m_ops = SDL_RWFromMem(m_octets.data(), m_octets.size());
+}
+
+BinaryIO::~BinaryIO() noexcept
+{
+    SDL_RWclose(m_ops);
+}
+
+BinaryIO::offset_t BinaryIO::seek(const BinaryIO::offset_t ofs)
+{
+    Sint64 new_ofs = SDL_RWseek(m_ops, ofs, RW_SEEK_SET);
+    if (new_ofs == -1) {
+        throwf("SDL_RWseek: %s", SDL_GetError());
+    }
+    return new_ofs;
+}
+
+BinaryIO::offset_t BinaryIO::tell() const
+{
+    Sint64 ofs = SDL_RWtell(m_ops);
+    if (ofs == -1) {
+        throwf("SDL_RWtell: %s", SDL_GetError());
+    }
+    return ofs;
+}
+
+std::int8_t BinaryIO::read_s8()
+{
+    std::int8_t data;
+    SDL_RWread(m_ops, &data, sizeof(data), 1);
+    return data;
+}
+
+std::uint8_t BinaryIO::read_u8()
+{
+    std::uint8_t data;
+    SDL_RWread(m_ops, &data, sizeof(data), 1);
+    return data;
+}
+
+std::int16_t BinaryIO::read_s16le()
+{
+    return SDL_ReadLE16(m_ops);
+}
+
+std::int16_t BinaryIO::read_s16be()
+{
+    return SDL_ReadBE16(m_ops);
+}
+
+std::uint16_t BinaryIO::read_u16le()
+{
+    return SDL_ReadLE16(m_ops);
+}
+
+std::uint16_t BinaryIO::read_u16be()
+{
+    return SDL_ReadBE16(m_ops);
+}
+
+std::int32_t BinaryIO::read_s32le()
+{
+    return SDL_ReadLE32(m_ops);
+}
+
+std::int32_t BinaryIO::read_s32be()
+{
+    return SDL_ReadBE32(m_ops);
+}
+
+std::uint32_t BinaryIO::read_u32le()
+{
+    return SDL_ReadLE32(m_ops);
+}
+
+std::uint32_t BinaryIO::read_u32be()
+{
+    return SDL_ReadBE32(m_ops);
+}
+
+std::int64_t BinaryIO::read_s64le()
+{
+    return SDL_ReadLE64(m_ops);
+}
+
+std::int64_t BinaryIO::read_s64be()
+{
+    return SDL_ReadBE64(m_ops);
+}
+
+std::uint64_t BinaryIO::read_u64le()
+{
+    return SDL_ReadLE64(m_ops);
+}
+
+std::uint64_t BinaryIO::read_u64be()
+{
+    return SDL_ReadBE64(m_ops);
+}
+
+float BinaryIO::read_f32le()
+{
+    // FIXME: Totally not portable
+
+    static_assert(sizeof(float) == 4, "sizeof(float) != 4 not supported");
+    std::uint8_t buf[4];
+    SDL_RWread(m_ops, buf, 1, 4);
+    float n;
+    std::memcpy(&n, buf, 4);
+    return n;
+}
+
+float BinaryIO::read_f32be()
+{
+    // FIXME: Totally not portable
+
+    static_assert(sizeof(float) == 4, "sizeof(float) != 4 not supported");
+    std::uint8_t buf[4];
+    SDL_RWread(m_ops, buf, 1, 4);
+    float n;
+    std::memcpy(&n, buf, 4);
+    return n;
+}
+
+double BinaryIO::read_f64le()
+{
+    // FIXME: Totally not portable
+
+    static_assert(sizeof(double) == 8, "sizeof(double) != 8 not supported");
+    std::uint8_t buf[8];
+    SDL_RWread(m_ops, buf, 1, 8);
+    double n;
+    std::memcpy(&n, buf, 8);
+    return n;
+}
+
+double BinaryIO::read_f64be()
+{
+    // FIXME: Totally not portable
+
+    static_assert(sizeof(double) == 8, "sizeof(double) != 8 not supported");
+    std::uint8_t buf[8];
+    SDL_RWread(m_ops, buf, 1, 8);
+    double n;
+    std::memcpy(&n, buf, 8);
+    return n;
+}
+
+void BinaryIO::read_chars(char* const ptr, const std::size_t size)
+{
+    SDL_RWread(m_ops, ptr, sizeof(char), size);
+}
+
+std::string BinaryIO::read_string(const std::size_t size)
+{
+    auto c_str = std::make_unique<char[]>(size);
+    read_chars(c_str.get(), size);
+    return std::string(c_str.get(), size);
+}
---
+++ src/binio.h
@@ -0,0 +1,61 @@
+#ifndef Q3BSP__BINIO_h
+#define Q3BSP__BINIO_H
+
+#include <vector>
+#include <string>
+#include <utility>
+#include <cstdint>
+
+struct SDL_RWops;
+
+class BinaryIO
+{
+    public:
+        using octet_vec_t = std::vector<std::uint8_t>;
+        using offset_t = typename octet_vec_t::size_type;
+
+        BinaryIO(octet_vec_t octets);
+        ~BinaryIO() noexcept;
+
+        BinaryIO(const BinaryIO&) = delete;
+        void operator=(const BinaryIO&) = delete;
+
+        offset_t seek(const offset_t ofs);
+        offset_t tell() const;
+
+        std::int8_t read_s8();
+        std::uint8_t read_u8();
+
+        std::int16_t read_s16le();
+        std::int16_t read_s16be();
+
+        std::uint16_t read_u16le();
+        std::uint16_t read_u16be();
+
+        std::int32_t read_s32le();
+        std::int32_t read_s32be();
+
+        std::uint32_t read_u32le();
+        std::uint32_t read_u32be();
+
+        std::int64_t read_s64le();
+        std::int64_t read_s64be();
+
+        std::uint64_t read_u64le();
+        std::uint64_t read_u64be();
+
+        float read_f32le();
+        float read_f32be();
+
+        double read_f64le();
+        double read_f64be();
+
+        void read_chars(char* const, const std::size_t);
+        std::string read_string(const std::size_t);
+
+    private:
+        octet_vec_t m_octets;
+        SDL_RWops*  m_ops;
+};
+
+#endif
