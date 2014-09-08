5edcd13683b85eb0984a208edad04e50fe7549c3
Dominik Prien <dprien@users.noreply.github.com>
2014-09-08 03:49:39 +0200

    Refactor main.cc, use vec3/vec4 when appropriate

--- src/main.cc
+++ src/main.cc
@@ -9,13 +9,14 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-#include "exception.h"
-#include "bsp.h"
-#include "vec.h"
-#include "archive.h"
-#include "tex.h"
-#include "frustum.h"
-#include "time.h"
+#include "src/exception.h"
+#include "src/bsp.h"
+#include "src/archive.h"
+#include "src/time.h"
+#include "src/math/vector3.h"
+#include "src/math/vector4.h"
+#include "src/math/matrix4.h"
+#include "src/math/util.h"
 
 class Render
 {
@@ -112,12 +113,12 @@ void Render::end_frame() const
 class Simulation
 {
     public:
-        CVec speed;
-        CVec position;
+        vec3 speed;
+        vec3 position;
 
-        void step(CVec dir, const float dt)
+        void step(vec3 dir, const float dt)
         {
-            const float m = dir.Magnitude();
+            const float m = dir.length();
             if (m > 1.0f) {
                 dir *= 1.0f / m;
             }
@@ -127,177 +128,170 @@ class Simulation
         }
 };
 
-void process_events(bool* done, Render* render, float* yaw, float* pitch, float*)
+namespace
 {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
-            case SDL_QUIT: {
-                *done = true;
-                break;
-            }
-            case SDL_WINDOWEVENT: {
-                switch (event.window.event) {
-                    case SDL_WINDOWEVENT_RESIZED: {
-                        render->resize(event.window.data1, event.window.data2);
-                        break;
-                    }
-                }
-                break;
-            }
-            case SDL_KEYUP: {
-                if (event.key.keysym.sym == SDLK_ESCAPE) {
+    void process_events(bool* done, Render* render, float* yaw, float* pitch,
+            float*)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
                     *done = true;
+                    break;
                 }
-                else if (event.key.keysym.sym == SDLK_f) {
-                    SDL_SetRelativeMouseMode(SDL_FALSE);
+                case SDL_WINDOWEVENT: {
+                    switch (event.window.event) {
+                        case SDL_WINDOWEVENT_RESIZED: {
+                            render->resize(event.window.data1,
+                                    event.window.data2);
+                            break;
+                        }
+                    }
+                    break;
                 }
-                break;
-            }
-            case SDL_MOUSEBUTTONDOWN: {
-                if (event.button.button == SDL_BUTTON_LEFT) {
-                    SDL_SetRelativeMouseMode(SDL_TRUE);
+                case SDL_KEYUP: {
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+                        *done = true;
+                    }
+                    else if (event.key.keysym.sym == SDLK_f) {
+                        SDL_SetRelativeMouseMode(SDL_FALSE);
+                    }
+                    break;
                 }
-                break;
-            }
-            case SDL_MOUSEMOTION: {
-                static int old_mode = SDL_GetRelativeMouseMode();
-                int cur_mode = SDL_GetRelativeMouseMode();
-                if (cur_mode != old_mode) {
-                    // After a switch, SDL2 generates a spurious
-                    // MOUSEMOTION event with *huge* deltas.
+                case SDL_MOUSEBUTTONDOWN: {
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        SDL_SetRelativeMouseMode(SDL_TRUE);
+                    }
+                    break;
                 }
-                else if (cur_mode) {
-                    *yaw += std::fmod(
-                            event.motion.xrel / 6.0f,
-                            360.0f);
-                    *pitch = std::min(
-                            90.0f,
-                            std::max(
-                                -90.0f,
-                                *pitch + event.motion.yrel / 6.0f));
+                case SDL_MOUSEMOTION: {
+                    static int old_mode = SDL_GetRelativeMouseMode();
+                    int cur_mode = SDL_GetRelativeMouseMode();
+                    if (cur_mode != old_mode) {
+                        // After a switch, SDL2 generates a spurious
+                        // MOUSEMOTION event with *huge* deltas.
+                    }
+                    else if (cur_mode) {
+                        float xr = event.motion.xrel / 6.0f;
+                        float yr = event.motion.yrel / 6.0f;
+                        *yaw += std::fmod(xr, 360.0f);
+                        *pitch = std::min(90.0f, std::max(-90.0f, *pitch + yr));
+                    }
+                    old_mode = cur_mode;
+                    break;
                 }
-                old_mode = cur_mode;
-                break;
             }
         }
     }
-}
 
-float step()
-{
-    static std::uint64_t total_frames = 0;
-    static std::uint64_t start_ticks = get_ticks();
-    static std::uint64_t last_update = 0;
+    float step()
+    {
+        static std::uint64_t total_frames = 0;
+        static std::int64_t start_ticks = get_ticks();
+        static std::int64_t last_update = 0;
 
-    static TickQueue tq;
-    float dt = tq.new_frame(75);
+        static TickQueue tq;
+        float dt = tq.new_frame(60);
 
-    ++total_frames;
-    std::uint64_t curr_ticks = get_ticks() - start_ticks;
-    if (get_ticks() - last_update >= TICKS_PER_SECOND / 10) {
-        float avg_fps = total_frames / (curr_ticks / float(TICKS_PER_SECOND));
-        std::printf(
-                "\r"
-                "%0.2f frames/sec, "
-                "%0.3f msec/frame, "
-                "%0.2f frames/sec avg  \b\b",
-                tq.get_frames_per_second(),
-                tq.get_seconds_per_frame() * 1000.0f,
-                avg_fps);
-        std::fflush(stdout);
-        last_update = get_ticks();
+        ++total_frames;
+#if 0
+        std::int64_t curr_ticks = get_ticks() - start_ticks;
+        if (get_ticks() - last_update >= TICKS_PER_SECOND / 10) {
+            float avg_fps = total_frames / (curr_ticks /
+                    float(TICKS_PER_SECOND));
+            std::printf(
+                    "\r"
+                    "%0.2f frames/sec, "
+                    "%0.3f msec/frame, "
+                    "%0.2f frames/sec avg  \b\b",
+                    tq.get_frames_per_second(),
+                    tq.get_seconds_per_frame() * 1000.0f,
+                    avg_fps);
+            std::fflush(stdout);
+            last_update = get_ticks();
+        }
+#endif
+
+        return dt;
     }
 
-    return dt;
-}
+    mat4 simulate(const float dt, const mat4& mdir, Simulation* sim)
+    {
+        const Uint8* keys = SDL_GetKeyboardState(nullptr);
+        vec4 movement;
+        if (keys[SDL_SCANCODE_W]) {
+            movement += vec4(0.0f, 0.0f, -1.0f) * mdir;
+        }
+        if (keys[SDL_SCANCODE_S]) {
+            movement += vec4(0.0f, 0.0f, 1.0f) * mdir;
+        }
+        if (keys[SDL_SCANCODE_A]) {
+            movement += vec4(-1.0f, 0.0f, 0.0f) * mdir;
+        }
+        if (keys[SDL_SCANCODE_D]) {
+            movement += vec4(1.0f, 0.0f, 0.0f) * mdir;
+        }
+        if (keys[SDL_SCANCODE_SPACE]) {
+            movement += vec4(0.0f, 1.0f, 0.0f);
+        }
+        if (keys[SDL_SCANCODE_LCTRL] || keys[SDL_SCANCODE_C]) {
+            movement += vec4(0.0f, -1.0f, 0.0f);
+        }
+        sim->step(vec3(movement), dt * 50.0f);
 
-CMat simulate(const float dt, const CMat& mdir, Simulation* sim)
-{
-    const Uint8* keys = SDL_GetKeyboardState(nullptr);
-    CVec movement;
-    if (keys[SDL_SCANCODE_W]) {
-        movement += CVec(0.0f, 0.0f, -1.0f) * mdir;
-    }
-    if (keys[SDL_SCANCODE_S]) {
-        movement += CVec(0.0f, 0.0f, 1.0f) * mdir;
-    }
-    if (keys[SDL_SCANCODE_A]) {
-        movement += CVec(-1.0f, 0.0f, 0.0f) * mdir;
-    }
-    if (keys[SDL_SCANCODE_D]) {
-        movement += CVec(1.0f, 0.0f, 0.0f) * mdir;
-    }
-    if (keys[SDL_SCANCODE_SPACE]) {
-        movement += CVec(0.0f, 1.0f, 0.0f);
-    }
-    if (keys[SDL_SCANCODE_LCTRL] || keys[SDL_SCANCODE_C]) {
-        movement += CVec(0.0f, -1.0f, 0.0f);
-    }
-
-    sim->step(movement, dt * 50.0f);
-    CMat mat;
-    MatTrans(mat, -sim->position.x, -sim->position.y, -sim->position.z);
-    return mat * mdir;
-}
-
-void loop(Render& render, const MapBSP46& map)
-{
-    float yaw = 0.0f, pitch = 0.0f, roll = 0.0f;
-    Simulation sim;
-
-    SDL_SetRelativeMouseMode(SDL_TRUE);
-
-    bool done = false;
-    while (!done) {
-        float dt = step();
-
-        process_events(&done, &render, &yaw, &pitch, &roll);
-        CMat mdir;
-        MatYRot(mdir, yaw);
-        MatXRot(mdir, pitch);
-        MatZRot(mdir, roll);
-        CMat mat = simulate(dt, mdir, &sim);
-
-        render.new_frame();
-        glMatrixMode(GL_MODELVIEW);
-        glLoadMatrixf(mat.GetMatrix());
-        map.draw(sim.position, CFrustum());
-        render.end_frame();
+        mat4 mat;
+        mat4_translate(mat, -sim->position.x, -sim->position.y, -sim->position.z);
+        return mat * mdir;
     }
 
-    printf("\n");
+    void loop(Render& render, const MapBSP46& map)
+    {
+        float yaw = 0.0f, pitch = 0.0f, roll = 0.0f;
+        Simulation sim;
+
+        SDL_SetRelativeMouseMode(SDL_TRUE);
+
+        bool done = false;
+        while (!done) {
+            float dt = step();
+
+            process_events(&done, &render, &yaw, &pitch, &roll);
+            mat4 mdir;
+            mat4_rotate_y(mdir, yaw);
+            mat4_rotate_x(mdir, pitch);
+            mat4_rotate_z(mdir, roll);
+            mat4 mat = simulate(dt, mdir, &sim);
+
+            render.new_frame();
+            glMatrixMode(GL_MODELVIEW);
+            glLoadMatrixf(mat.get_floats());
+            map.draw(sim.position, GLFrustum<float>());
+            render.end_frame();
+        }
+
+        printf("\n");
+    }
 }
 
 int main(int argc, char* argv[])
 {
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] <<
-            " <path> <map> [<overbright bits>]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <path> <map>" << std::endl;
         return 1;
     }
-    if (argc >= 4) {
-        int overbrightBits = atoi(argv[3]);
-        if (overbrightBits < 0 || overbrightBits > 8) {
-            std::cerr << argv[0] << ": overbright bits: allowed range is 0-8" <<
-                std::endl;
-            return 1;
-        }
-        CLightmapTex::SetOverbrightBits(overbrightBits);
-    }
+    std::string bsp_filename = "maps/";
+    bsp_filename += argv[2];
+    bsp_filename += ".bsp";
 
     SDL_Init(SDL_INIT_EVERYTHING);
-
     try {
         Uint32 mticks = SDL_GetTicks();
         PAK3Archive pak(argv[1]);
 
-        Render render(1440, 900);
-
-        std::string filename = "maps/";
-        filename += argv[2];
-        filename += ".bsp";
-        MapBSP46 map(filename.c_str(), pak);
+        /* Render render(1440, 900); */
+        Render render(1440, 800);
+        MapBSP46 map(bsp_filename.c_str(), pak);
 
         std::printf("Init: %0.2f sec\n", (SDL_GetTicks() - mticks) / 1000.0f);
         loop(render, map);
@@ -310,9 +304,6 @@ int main(int argc, char* argv[])
         std::cerr << argv[0] << ": Error: Unknown exception" << std::endl;
         return 1;
     }
-
-    CTexManager::Destroy();
     SDL_Quit();
-
     return 0;
 }
